76e8edbc34ffe5bb776f10f46b4cbff60872937f
Grace Cho <48530228+hellogracecho@users.noreply.github.com>
2021-02-22 22:05:23 -0800

    useReduer work in progress

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,42 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState('');
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newPerson = { id: new Date().getTime().toString(), name };
+      setShowModal(true);
+      setPeople([...people, newPerson]);
+      setName('');
+    } else {
+      setShowModal(true);
+    }
+  };
+
+  return (
+    <>
+      {showModal && <Modal />}
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button type='submit'>Add</button>
+      </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id} className='item'>
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Index;
