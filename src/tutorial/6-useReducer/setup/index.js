94010f1ddea26453044cb28816b466f2fd1be873
Grace Cho <48530228+hellogracecho@users.noreply.github.com>
2021-02-25 21:08:59 -0800

    useReducer practice

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,28 +1,54 @@
 import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
-// reducer function
+// ** reducer function: this is where we deal with state
+const reducer = (state, action) => {
+  console.log(action);
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload];
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'Item added',
+    };
+  }
+  if (action.type === 'NO_VALUE') {
+    return {
+      ...state,
+      isModalOpen: true,
+      modalContent: 'Please eneter a value',
+    };
+  }
+  throw new Error('no matching action type');
+};
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+};
 
 const Index = () => {
   const [name, setName] = useState('');
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  // const [people, setPeople] = useState(data);
+  // const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
       const newPerson = { id: new Date().getTime().toString(), name };
-      setShowModal(true);
-      setPeople([...people, newPerson]);
+      dispatch({ type: 'ADD_ITEM', payload: newPerson }); // {type: ''} common practice with uppercase
       setName('');
     } else {
-      setShowModal(true);
+      // no value
+      dispatch({ type: 'NO_VALUE' });
     }
   };
 
   return (
     <>
-      {showModal && <Modal />}
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
       <form className='form' onSubmit={handleSubmit}>
         <input
           type='text'
@@ -31,7 +57,7 @@ const Index = () => {
         />
         <button type='submit'>Add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
           <div key={person.id} className='item'>
             <h4>{person.name}</h4>
