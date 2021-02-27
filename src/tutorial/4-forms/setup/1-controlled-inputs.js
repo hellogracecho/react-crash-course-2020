62a64e285454f257e60bb6f97e8293cd2b4a5c05
Grace Cho <hellogracec@gmail.com>
2021-02-27 10:36:43 -0800

    commit

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,10 +8,21 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(firstName, email);
-  }
+    if (firstName && email) { // form validation
+      const person = {id: new Date().getTime().toString(), firstName, email};
+      setPeople((people) => {
+        return [...people, person]
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('empty value..');
+    }
+  };
 
   return <>
   <article>
@@ -27,6 +38,15 @@ const ControlledInputs = () => {
       <button type="submit">Add person</button>
       {/* <button type="submit" onClick={handleSubmit}>Add person</button> */}
     </form>
+    {
+      people.map((person) => {
+        const {id, firstName, email} = person;
+        return <div key={id} className="item">
+          <h4>{firstName}</h4>
+          <p>{email}</p>
+        </div>
+      })
+    }
   </article>
   </>;
 };
